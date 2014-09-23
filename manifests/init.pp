fed034d468a26b4318c03814a22d0d69610770ed
Tom Boland <tom.boland@fasthosts.com>
2014-09-23 10:22:04 +0100

    making sure razor homedir is created with same owner as

--- manifests/init.pp
+++ manifests/init.pp
@@ -577,9 +577,16 @@ class spamassassin(
       alias    => 'razor',
       require  => Package['spamassassin'],
     } ->
-    file { $final_razor_home:
-      ensure => directory,
-      recurse => true,
+    if $run_execs_as_user {
+      file { $final_razor_home:
+        owner   => $run_execs_as_user,
+        ensure  => directory,
+        recurse => true,
+    } else {
+      file { $final_razor_home:
+        ensure  => directory,
+        recurse => true,
+      }  
     } ->
     exec { 'razor_register':
       command => "/usr/bin/razor-admin -home=${final_razor_home} -register",
